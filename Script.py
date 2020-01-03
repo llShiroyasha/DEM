570cf823ba8d6ab655006fad02d7ca2aa013ab3f
llShiroyasha <robin.porquet@gmail.com>
2020-01-03 14:36:26 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,8 +15,27 @@ class POBJ:
     def IsActive(self):
         return True
     def GetResources(self):
+        return {'Pixmap':'', 'MenuText':'FCStd > POBJ Converter','ToolTip':'FCStd to POBJ Converter'}
+        
+class VOBJ:
+    def Activated(self):
+        import demconverters.FCstdVOBJconverter as FCstdVOBJconverter
+    def IsActive(self):
+        return True
+    def GetResources(self):
         return {'Pixmap':'', 'MenuText':'FCStd > VOBJ Converter','ToolTip':'FCStd to VOBJ Converter'}
+    
+        
+class Material:
+    def Activated(self):
+        import demconverters.Materialfiles as Materialfiles
+    def IsActive(self):
+        return True
+    def GetResources(self):
+        return {'Pixmap':'', 'MenuText':'FCStd > Material', "ToolTip":"FCStd to Material files"}
        
 FreeCADGui.addCommand('WOBJ', WOBJ())
 FreeCADGui.addCommand('POBJ', POBJ())
+FreeCADGui.addCommand('VOBJ', VOBJ())
+FreeCADGui.addCommand("Material",Material())
 
