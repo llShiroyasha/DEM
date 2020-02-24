29e90d227273ec0e5dce92c21e7c31dab6e26a58
llShiroyasha <robin.porquet@gmail.com>
2020-02-24 18:11:13 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,32 @@
+# ***************************************************************************
+# *   (c) Robin PORQUET (robin.porquet@gmail.com) 2019                      *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# *   Made during my internship in South Africa                             *
+# *   Director of my internship: Pr. Daniel N. Wilke                        *
+# *   Tutor in France: Pr. Patrick Pizette                                  *       
+# *                                                                         *
+# *   Robin PORQUET 2019                                                    *
+# ***************************************************************************
+
+
 import FreeCAD, FreeCADGui
 
 class WOBJ:
